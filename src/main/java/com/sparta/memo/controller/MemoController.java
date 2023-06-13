3e0cb1d3c858ed03be70a9470ff8c73dd8872762
azuressu <martina0130@naver.com>
2023-06-13 11:55:49 +0900

    Update와 Delete 구현

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -1,4 +1,5 @@
 package com.sparta.memo.controller;
+
 import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
@@ -31,14 +32,42 @@ public class MemoController {
         return memoResponseDto;
     }
 
-
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemos() { // list인 이유는 메모가 하나일 수는 없기 때문 !
         // Map to List
         List<MemoResponseDto> responseList = memoList.values().stream()
-                                             .map(MemoResponseDto::new).toList(); // :: new를 하면 생성자(Memo를 파라미터로 갖는)가 사용됨
+                .map(MemoResponseDto::new).toList(); // :: new를 하면 생성자(Memo를 파라미터로 갖는)가 사용됨
 
         return responseList;
+    }
+
+    @PutMapping("/memos/{id}") // path valuable 방식
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        // 업데이트할 메모의 id, 수정할 내용을 requestbody로 받아옴
+        // 메모가 실제로 DB에 존재하는지 확인하기
+        if (memoList.containsKey(id)) {
+            // 해당 메모를 가져오기
+            Memo memo = memoList.get(id); // map 자료구조에서 그 id에 맞는 메모 객체가 반환될 것임
+
+            // 가져온 메모를 수정하기
+            memo.update(requestDto);
+
+            return memo.getId(); // 혹은 return id; 도 가능
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다/");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) { // 지우는 것이니 RequestDto는 필요없음
+        // 해당 메모가 데이터베이스에 존재하는지 확인하기
+        if (memoList.containsKey(id)) {
+            // 해당 메모를 삭제하기 (key를 넣어주면 가능)
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다");
+        }
     }
 
 
