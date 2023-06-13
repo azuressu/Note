aa0e176a437460e2dd44c63aafaaf9e8293298dd
azuressu <martina0130@naver.com>
2023-06-13 17:59:46 +0900

    역할 분리하기 (Controller, Service, Repository)

---
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -0,0 +1,64 @@
+package com.sparta.memo.service;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.entity.Memo;
+import com.sparta.memo.repository.MemoRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class MemoService {
+    private final JdbcTemplate jdbcTemplate;
+
+    public MemoService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    // 데이터를 반환 ..? 하는 부분 ?
+    public MemoResponseDto createMemo(MemoRequestDto requestDto) {
+        // RequestDto -> Entity
+        Memo memo = new Memo(requestDto);
+
+        // DB 저장
+        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
+        Memo saveMemo = memoRepository.save(memo); // 데이터베이스에 연결하는 거니까 메소드명 동일하게 해주지 않아도 됨
+
+        // Entity -> ResponseDto
+        MemoResponseDto memoResponseDto = new MemoResponseDto(saveMemo); // 그냥 memo 인가 ?
+
+        return memoResponseDto;
+    }
+
+    public List<MemoResponseDto> getMemos() {
+        // DB 조회
+        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
+        return memoRepository.findAll(); // 전달해주는 파라미터 필요없음
+    }
+
+    public Long updateMemo(Long id, MemoRequestDto requestDto) {
+        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
+        // 해당 메모가 DB에 존재하는지 확인
+        Memo memo = memoRepository.findById(id);  // 접근 제어자 주의하기
+        if(memo != null) {
+            // memo 내용 수정
+            memoRepository.update(id, requestDto);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    } // updateMemo
+
+    public Long deleteMemo(Long id) {
+        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
+        // 해당 메모가 DB에 존재하는지 확인
+        Memo memo = memoRepository.findById(id);
+        if(memo != null) {
+            // memo 삭제
+            memoRepository.delete(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    } // deleteMemo
+
+}
